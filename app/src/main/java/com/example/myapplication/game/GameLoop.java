ce0316b335ee2dc4c97924887639aecd098ac2f3
lyxxxnb <niclas.bader@studium.uni-hamburg.de>
2024-08-14 17:17:15 +0200

    Test Change

--- app/src/main/java/com/example/myapplication/game/GameLoop.java
+++ app/src/main/java/com/example/myapplication/game/GameLoop.java
@@ -49,6 +49,7 @@ public class GameLoop
         double lastFrame = System.nanoTime();
         double now;
 
+        // Test Kommentar für Git
         while (_gameState == GameStates.RUNNING)
         {
             now = System.nanoTime();
